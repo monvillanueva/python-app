e32e9583b570c00f658fad3273c3ac84d15dc49a
monica-villanueva <63138869+monica-villanueva@users.noreply.github.com>
2026-04-14 16:45:10 -0700

    fix shortened sha

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         {
             'time': str(datetime.now().isoformat()),
             'hostname': socket.gethostname(),
-            'message': 'You are doing great, human! Keep going!'
+            'message': 'You are doing great, human. Keep going!'
         }
     )
 
